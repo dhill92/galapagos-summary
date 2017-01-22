ae298a5b11a96274ee015bc0e71c4e85e671bc69
dhill92 <dhill92@gmail.com>
2017-01-22 16:52:57 -0500

    finished assignment

---
+++ galapagos-assn.rb
@@ -0,0 +1,15 @@
+#this is my first assignment
+
+date_of_departure = "march 10"
+date_of_return = "april 17"
+number_of_tortoises = 12
+name_of_beach = "cape cod"
+name_of_hotel = "Best Western"
+number_of_iguanas = 56
+number_of_animals = number_of_tortoises + number_of_iguanas
+
+text = "We left #{date_of_departure} for The Galapagos Islands and stayed at the #{name_of_hotel} hotel. During
+the trip I saw over #{number_of_animals} animals. My favorite beach during the trip was #{name_of_beach}.
+After an amazing trip we returned on #{date_of_return}.  Can't wait to go back!"
+
+puts text
